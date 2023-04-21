dbc665a398693c7df3b106fc7f34d36756959714
staillansag <103417302+staillansag@users.noreply.github.com>
2023-04-21 21:05:19 +0200

    .

--- resources/buildScripts/02.connectAKS.sh
+++ resources/buildScripts/02.connectAKS.sh
@@ -3,7 +3,7 @@
 echo "Connecting to Azure with SP ${AZ_SP_ID}"
 az login --service-principal -u ${AZ_SP_ID} -p ${AZ_SP_SECRET} --tenant ${AZ_TENANT_ID} || exit 1
 
-echo "Retrieving kubeconfig file for cluster ${CLUSTER_NAME}"
-az aks get-credentials --resource-group ${RESOURCE_GROUP} --name ${CLUSTER_NAME} --overwrite-existing || exit 1
+echo "Retrieving kubeconfig file for cluster ${AKS_CLUSTER_NAME}"
+az aks get-credentials --resource-group ${AKS_RESOURCE_GROUP} --name ${AKS_CLUSTER_NAME} --overwrite-existing || exit 1
 
 kubectl get nodes || exit 1
