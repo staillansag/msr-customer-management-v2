4a8a702b0a729ebf3812c02684e293bee9597577
staillansag <103417302+staillansag@users.noreply.github.com>
2023-06-08 18:22:32 +0200

    Ditto

--- resources/buildScripts/automatedTests.sh
+++ resources/buildScripts/automatedTests.sh
@@ -2,8 +2,6 @@
 
 sed 's#REPLACEME_API_ENDPOINT#'${API_ENDPOINT}'#g' ./resources/test/newman/CustomerManagement-environment-template.json | sed 's#REPLACEME_API_KEY#'${API_KEY}'#g' > env
 
-cat env
-
 newman run ./resources/test/newman/CustomerManagement.postman_collection.json \
   -e env || exit 8
 
